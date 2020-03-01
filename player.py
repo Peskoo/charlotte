71cc09b652c60c5a4727a21a75dbf46f5c553c1a
Peskoo <pesko@lasalledutemps.fr>
2020-03-01 14:30:33 +0100

    player added

---
+++ player.py
@@ -0,0 +1,20 @@
+from pygame import mixer
+
+
+mixer.init()
+mixer.music.load('CherieFM.mp3')
+mixer.music.set_volume(0.7)
+mixer.music.play()
+
+while True:
+    print('press p to pause, r to resume')
+    print('press e to exit')
+    query = input('>>>> ')
+
+    if query == 'p':
+        mixer.music.pause()
+    elif query == 'r':
+        mixer.music.unpause()
+    elif query == 'e':
+        mixer.music.stop()
+        break
